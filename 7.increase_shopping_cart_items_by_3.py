3c952e6d61a6faf791079b4a81e7408b34f0db81
nintusing <nintusingh30@gmail.com>
2026-05-07 23:03:02 +0530

    update

---
+++ 7.increase_shopping_cart_items_by_3.py
@@ -0,0 +1,9 @@
+#7.Increase shopping cart items by 3
+
+Before_cart_items = 5
+Increase_cart_items = 3
+print(f"Before_cart_items : {Before_cart_items}")
+print(f"Increase_cart_items : {Increase_cart_items}")
+
+After_cart_items = Before_cart_items + Increase_cart_items
+print(f"After_cart_items : {After_cart_items}")
