c47ea274b74fb921fc3f3f736bc48575ff79141b
nintusing <nintusingh30@gmail.com>
2026-05-07 22:51:49 +0530

    update

---
+++ 5.Update_balance_after_withdrawal.py
@@ -0,0 +1,7 @@
+Before_balance = 12000
+withdrawl = 3000
+print(f"Before_balance : {Before_balance}")
+print(f"withdrawl : {withdrawl}")
+
+After_withdrawl = Before_balance - withdrawl
+print(f"After_withdrawl : {After_withdrawl}")
