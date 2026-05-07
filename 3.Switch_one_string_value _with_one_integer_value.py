42cc1550081616a652ddbf57ffc35b10c48701aa
nintusing <nintusingh30@gmail.com>
2026-05-07 22:44:23 +0530

    update

---
+++ 3.Switch_one_string_value _with_one_integer_value.py
@@ -0,0 +1,10 @@
+n1 = 200
+char2 = "java"
+print(f"Before swap n1 : {n1}")
+print(f"Before swap char2 : {char2}")
+
+temp = n1
+n1 = char2
+char2 = n1
+print(f"After swap n1 : {n1}")
+print(f"After swap char2 : {char2}")
