5fb7db1c08eeeddff9a56696b6efdf84a6d40616
nintusing <nintusingh30@gmail.com>
2026-05-07 22:17:01 +0530

    update

---
+++ Switch-values-of-two-integers.py
@@ -0,0 +1,13 @@
+#01. Switch values of two integers
+
+n1 = 20
+n2 = 30
+print(f"Before swap n1 : {n1}")
+print(f"Before swap n2 : {n2}")
+
+x = n1
+n1 =n2
+n2 = x
+print(f"After swap n1 : {n1}")
+print(f"After swap n2 : {n2}")
+
