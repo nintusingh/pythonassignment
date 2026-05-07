04511aca24018dc54113af4b599eb0b8555b6d97
nintusing <nintusingh30@gmail.com>
2026-05-07 22:29:24 +0530

    update

---
+++ 2.Switch_values_of_two_strings.py
@@ -0,0 +1,10 @@
+char1 = "hello"
+char2 = "java"
+print(f"Before swap cahr1 : {char1}")
+print(f"Before swap char2 : {char2}")
+
+temp = char1
+char1 = char2
+char2 = temp
+print(f"After swap char1 : {char1}")
+print(f"After swap char2 : {char2}")
