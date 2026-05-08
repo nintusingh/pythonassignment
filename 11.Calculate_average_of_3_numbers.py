802fd3223d2848448ab6cf9290e95d2374a032f2
nintusing <nintusingh30@gmail.com>
2026-05-08 07:06:35 +0530

    update

---
+++ 11.Calculate_average_of_3_numbers.py
@@ -0,0 +1,9 @@
+num1 = 25
+num2 = 35
+num3 = 45
+print(f"num1 : {num1}")
+print(f"num2 : {num2}")
+print(f"num3 : {num3}")
+Total = num1 + num2 + num3
+average = Total / 3
+print(f"average : {average}")
