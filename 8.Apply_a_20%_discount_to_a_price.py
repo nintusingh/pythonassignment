c47261adb09788a72c8af5080c8108523a1e8a64
nintusing <nintusingh30@gmail.com>
2026-05-08 06:31:41 +0530

    update

---
+++ 8.Apply_a_20%_discount_to_a_price.py
@@ -0,0 +1,7 @@
+Before_price = 1000
+Discount = 20
+print(f"Before_price : {Before_price}")
+print(f"Discount : {Discount}")
+
+percentage = Before_price - (Before_price * Discount/100)
+print(f"After_discount_price : {percentage}")
