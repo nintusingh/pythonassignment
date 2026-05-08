75e05fcdabf9c96af6bd782e79296d64f0d7e1a6
nintusing <nintusingh30@gmail.com>
2026-05-08 06:40:46 +0530

    update

---
+++ 9.Calculate_student_percentage.py
@@ -0,0 +1,7 @@
+Obtain_marks = 430
+Total_marks = 500
+print(f"Obtain_marks : {Obtain_marks}")
+print(f"Total_marks : {Total_marks}")
+
+percentage = (Obtain_marks / Total_marks) * 100
+print(f"Percentage : {percentage}%")
