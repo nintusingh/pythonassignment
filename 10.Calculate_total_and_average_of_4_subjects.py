4b0aa4ee0909307f2c8a13d818038e309bbeb078
nintusing <nintusingh30@gmail.com>
2026-05-08 06:51:57 +0530

    update

---
+++ 10.Calculate_total_and_average_of_4_subjects.py
@@ -0,0 +1,12 @@
+Subject_1 = 80
+Subject_2 = 75
+Subject_3 = 90
+Subject_4 = 85
+print(f"Subject_1 : {Subject_1}")
+print(f"Subject_2 : {Subject_2}")
+print(f"Subject_3 : {Subject_3}")
+print(f"Subject_4 : {Subject_4}")
+Total_marks = Subject_1 + Subject_2 + Subject_3 + Subject_4
+print(f"Total_marks : {Total_marks}")
+Average_marks = Total_marks / 4
+print(f"Average_marks : {Average_marks}")
