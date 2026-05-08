56e5db7ae222ea2b88b52b09872e70fc3f818cc4
nintusing <nintusingh30@gmail.com>
2026-05-08 11:38:49 +0530

    update

---
+++ 13.Calculate_simple_interest.py
@@ -0,0 +1,11 @@
+#. Calculate simple interest
+
+principal = 10000
+rate = 5
+time = 2
+print(f"principal : {principal}")
+print(f"rate : {rate}")
+print(f"time : {time}")
+si = int((principal * rate * time) / 100)
+print(f"simple intrest : {si}")
+
---
+++ 8-may.py
@@ -0,0 +1,9 @@
+#1.for loop
+# range (start:0,stop-1,step:1)
+
+#print("aman"*10)
+
+#for i in range(10):
+#range always work with integer number
+for i in range(1,15,2)
+print(i)
